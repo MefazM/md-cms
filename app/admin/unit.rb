cd7738dcc789b636214a281000f3f7d615a2d2eb
Roman <r.tomilin@111minutes.com>
2013-10-15 17:31:54 +0300

    Refactoring unit index page.

--- app/admin/unit.rb
+++ app/admin/unit.rb
@@ -1,3 +1,30 @@
 ActiveAdmin.register Unit do
+  config.filters = false
+
   form :partial => "form"
+
+  index do
+    selectable_column
+
+    column :name
+    column :health_points
+    column :movement_speed
+    column :range_attack
+    column :melee_attack
+
+    column :resist_type do |type|
+      
+      type.resist_type.nil? ? 'none' : Unit::ATTACK_TYPES[type.resist_type]
+    end
+
+    column :melee_attack_damage_type do |type|
+      type.melee_attack_damage_type.nil? ? '-' : Unit::ATTACK_TYPES[type.melee_attack_damage_type]
+    end
+
+    column :range_attack_damage_type do |type|
+      type.range_attack_damage_type.nil? ? '-' : Unit::ATTACK_TYPES[type.range_attack_damage_type]
+    end
+
+    default_actions
+  end
 end
