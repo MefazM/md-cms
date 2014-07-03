65c2f5c7a488b4d399b779f845e92b09d794c6f9
mr.The <mmr.the@gmail.com>
2014-07-03 14:36:02 +0300

    Add config editor

---
+++ app/admin/game_settings.rb
@@ -0,0 +1,34 @@
+ActiveAdmin.register_page "YAML Game Settings" do
+
+  page_action :update, method: :post do
+    current_file = Rails.root + 'config/game_settings/game_settings.json'
+    old_file = Rails.root + "config/game_settings/game_settings.#{Time.now.to_i}.json"
+
+    FileUtils.mv current_file, old_file
+
+    json = params[:game_settings][:json]
+
+    File.open(current_file, 'w+') { |file| file.puts json }
+
+    flash[:notice] = "Updated!"
+    redirect_to :admin_yaml_game_settings
+  end
+
+  content do
+    versions = Dir["config/game_settings/*"].map{ |f| f.split('/').last }.compact
+
+    @versions_select = versions.map do |version|
+      humanized = Time.at( version.match(/(\d+)/)[1].to_i ) rescue 'current'
+
+      [humanized, version]
+    end
+
+    @version = 'game_settings.json'
+    @version = params[:version] if versions.include? params[:version]
+
+    @game_settings_json = File.read(Rails.root + "config/game_settings/#{@version}")
+    render partial: "piu", locals: { game_settings_json: @game_settings_json, versions: @versions_select, version: @version }
+  end
+
+
+end
